8d7c8309b628039397093def7f3ba72081ed0fac
Sori Kim <ksr@gabiacns.com>
2022-01-12 14:10:50 +0900

    영문 폰트 추가, 툴팁/토글 기능 개선

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -12,35 +12,85 @@ document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector("header");
 
   /* =====================================================
+       Toggle
+  ===================================================== */
+  const toggles = document.querySelectorAll(".toggle");
+  if (toggles) {
+    toggles.forEach((toggle) => {
+      toggle.addEventListener("click", (event) => {
+        const button = event.target;
+        button.classList.toggle("is-active");
+      });
+    });
+  }
+
+  /* =====================================================
+       Tooltip
+  ===================================================== */
+  const tooltips = document.querySelectorAll(".ico-tooltip");
+  if (tooltips) {
+    tooltips.forEach((tooltip) => {
+      tooltip.addEventListener("click", (event) => {
+        const content = event.target.nextElementSibling;
+        content.classList.toggle("is-active");
+      });
+    });
+  }
+
+  /* =====================================================
        Section Keyword History: List Scroll
   ===================================================== */
-  const sectionKeywordHistory = document.querySelector(".section-keyword-history");
+  const sectionKeywordHistory = document.querySelector(
+    ".section-keyword-history"
+  );
   var scrollVertical01 = document.querySelector(".scroll-vertical-01");
   var scrollVertical02 = document.querySelector(".scroll-vertical-02");
   var scrollHorizontal01 = document.querySelector(".scroll-horizontal-01");
   var scrollHorizontal02 = document.querySelector(".scroll-horizontal-02");
 
   if (sectionKeywordHistory) {
-
     const handleScrollVertical01 = () => {
       scrollVertical02.scrollTop = scrollVertical01.scrollTop;
-    }
+    };
 
     const handleScrollVertical02 = () => {
       scrollVertical01.scrollTop = scrollVertical02.scrollTop;
-    }
+    };
 
     const handleScrollHorizontal01 = () => {
       scrollHorizontal02.scrollLeft = scrollHorizontal01.scrollLeft;
-    }
+    };
 
     const handleScrollHorizontal02 = () => {
       scrollHorizontal01.scrollLeft = scrollHorizontal02.scrollLeft;
-    }
+    };
 
     scrollVertical01.addEventListener("scroll", handleScrollVertical01);
     scrollVertical02.addEventListener("scroll", handleScrollVertical02);
     scrollHorizontal01.addEventListener("scroll", handleScrollHorizontal01);
     scrollHorizontal02.addEventListener("scroll", handleScrollHorizontal02);
+  }
+
+  /* =====================================================
+         Section Keyword History: Notification
+    ===================================================== */
+  const notificationDetails = document.querySelector(".notification-details");
+
+  if (notificationDetails) {
+    const notifications = document.querySelectorAll(".notification");
+    notifications.forEach((notification) => {
+      notification.addEventListener("click", (event) => {
+        const details = event.target.nextElementSibling;
+        details.classList.toggle("is-active");
+      });
+    });
+
+    const closeButtons = document.querySelectorAll(".close");
+    closeButtons.forEach((close) => {
+      close.addEventListener("click", (event) => {
+        const modal = event.target.closest("article");
+        modal.classList.remove("is-active");
+      });
+    });
   }
 });
