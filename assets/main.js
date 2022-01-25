7f5d8345b2ef7230a6ef867ec5baa57ed76f0a1d
Sori Kim <ksr@gabiacns.com>
2022-01-25 16:17:51 +0900

    Update 2022-01-25T07:17:48.593Z

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,279 @@
-!function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var f="function"==typeof require&&require;if(!u&&f)return f(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n||e)},f,f.exports,e,t,n,r)}return n[o].exports}for(var i="function"==typeof require&&require,o=0;o<r.length;o++)s(r[o]);return s}({1:[function(require,module,exports){"use strict";document.addEventListener("DOMContentLoaded",function(){var closeButtons=.01*window.innerHeight;document.documentElement.style.setProperty("--vh","".concat(closeButtons,"px")),window.addEventListener("resize",function(){var vh=.01*window.innerHeight;document.documentElement.style.setProperty("--vh","".concat(vh,"px"))});closeButtons=document.querySelectorAll("input[type='search'], input[type='url']");closeButtons&&closeButtons.forEach(function(input){input.nextElementSibling.addEventListener("click",function(event){event.target.previousElementSibling.value=""})});closeButtons=document.querySelectorAll(".toggle");closeButtons&&closeButtons.forEach(function(toggle){toggle.addEventListener("click",function(event){event.target.classList.toggle("is-active")})});var dataToggles=document.querySelectorAll("[data-toggle]");dataToggles&&document.querySelectorAll(".toggle-controller").forEach(function(controller){controller.addEventListener("click",function(event){dataToggles.forEach(function(toggle){event.target.dataset.toggle==toggle.dataset.toggle&&(toggle.classList=controller.classList)})})});closeButtons=document.querySelectorAll(".ico-tooltip");closeButtons&&closeButtons.forEach(function(tooltip){tooltip.addEventListener("click",function(event){event.target.nextElementSibling.classList.toggle("is-active")})});closeButtons=document.querySelectorAll(".check-all");closeButtons&&closeButtons.forEach(function(all){all.addEventListener("click",function(inputName){inputName=inputName.target.getAttribute("name");document.getElementsByName(inputName).forEach(function(checkbox){checkbox.checked=all.checked})})});var alertModal,alertModalContent,confirmModal,confirmModalContent,initialized,modals=document.querySelectorAll(".modal");modals[0]&&(alertModal=document.querySelector(".modal-alert"),alertModalContent=document.querySelector(".modal-alert .modal-content"),confirmModal=document.querySelector(".modal-confirm"),confirmModalContent=confirmModal.querySelector(".modal-content"),initialized=function(){modals.forEach(function(modal){modal.classList.remove("is-active")})},(closeButtons=document.querySelectorAll("[data-modal]"))&&closeButtons.forEach(function(button){button.addEventListener("click",function(modalName){initialized();modalName=modalName.target.dataset.modal;document.querySelector(".".concat(modalName)).classList.add("is-active")})}),confirmModal&&document.querySelectorAll("[data-modal-confirm]").forEach(function(button){button.addEventListener("click",function(modalData){modalData=modalData.target.dataset.modalConfirm;confirmModal.querySelector(".btn-submit").setAttribute("data-modal-alert",modalData),confirmModal.classList.add("is-active"),document.querySelectorAll("[data-modal-alert]").forEach(function(button){button.addEventListener("click",function(modalData){initialized();modalData=modalData.target.dataset.modalAlert;alertModal.classList.add("is-active"),"check-delete-keyword"!==modalData&&"delete-keyword"!==modalData||(alertModalContent.innerHTML="<p>키워드 삭제가 완료되었습니다</p>"),"check-delete-group"===modalData&&(alertModalContent.innerHTML="<p>그룹 삭제가 완료되었습니다.</p>"),"select-group"===modalData&&(alertModalContent.innerHTML="<p>그룹지정이 완료되었습니다.</p>")})}),"check-delete-keyword"===modalData&&(confirmModalContent.innerHTML="<p>키워드 삭제 후 적용하시면 삭제된 키워드와 관련된 데이터가 있는 경우 모두 삭제됩니다. 재등록시에도 삭제된 데이터는 복구되지 않습니다. 삭제를 진행하시겠습니까?</p>"),"delete-keyword"===modalData&&(confirmModalContent.innerHTML="<p>키워드 삭제 시 해당 키워드의 모든 데이터가 삭제됩니다. 재등록시에도 삭제된 데이터는 복구되지 않습니다. 삭제를 진행하시겠습니까?</p>"),"check-delete-group"===modalData&&(confirmModalContent.innerHTML="<p>그룹 삭제 시 그룹에 속한 키워드는 그룹이 해제됩니다. 그룹 삭제를 진행하시겠습니까?</p>"),"select-group"===modalData&&(confirmModalContent.innerHTML="<p>이미 그룹이 지정된 키워드가 있습니다. 그룹을 지정하실 경우 기존 그룹이 새로 지정된 그룹으로 대체됩니다. 그룹지정을 진행하시겠습니까?</p>")})}),alertModal&&document.querySelectorAll("[data-modal-alert]").forEach(function(button){button.addEventListener("click",function(modalData){initialized();modalData=modalData.target.dataset.modalAlert;alertModal.classList.add("is-active"),console.log(modalData),"ungroup"===modalData&&(alertModalContent.innerHTML="<p>그룹해제가 완료되었습니다.</p>"),"create-keyword"===modalData&&(alertModalContent.innerHTML="<p>키워드 등록이 완료되었습니다.</p>"),"edit-group"!==modalData&&"search-group"!==modalData||(alertModalContent.innerHTML="<p>적용이 완료되었습니다.</p>")})}),(closeButtons=document.querySelectorAll(".close"))&&closeButtons.forEach(function(close){close.addEventListener("click",function(event){event.target.closest(".modal").classList.remove("is-active"),alertModal&&(alertModalContent.innerHTML=""),confirmModal&&(confirmModalContent.innerHTML="")})}));var scrollVertical01,scrollVertical02,scrollHorizontal01,scrollHorizontal02,advancedSearch=document.querySelector(".advanced-search");advancedSearch&&document.querySelector(".btn-advanced-search").addEventListener("click",function(){advancedSearch.classList.toggle("is-active")}),document.querySelector(".list.scroll")&&(scrollVertical01=document.querySelector(".scroll-vertical-01"),scrollVertical02=document.querySelector(".scroll-vertical-02"),scrollHorizontal01=document.querySelector(".scroll-horizontal-01"),scrollHorizontal02=document.querySelector(".scroll-horizontal-02"),scrollVertical01&&scrollVertical02.addEventListener("scroll",function(){scrollVertical01.scrollTop=scrollVertical02.scrollTop}),scrollHorizontal01&&scrollHorizontal02.addEventListener("scroll",function(){scrollHorizontal01.scrollLeft=scrollHorizontal02.scrollLeft})),document.querySelector(".notification-details")&&(document.querySelectorAll(".notification").forEach(function(notification){notification.addEventListener("click",function(event){event.target.nextElementSibling.classList.toggle("is-active")})}),document.querySelectorAll(".close").forEach(function(close){close.addEventListener("click",function(event){event.target.closest("article").classList.remove("is-active")})}))})},{}]},{},[1]);
+document.addEventListener("DOMContentLoaded", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
+  const main = document.querySelector("main");
+
+  /* =====================================================
+       Input Search Close Button
+  ===================================================== */
+  const searchInputs = document.querySelectorAll(
+    "input[type='search'], input[type='url']"
+  );
+  if (searchInputs) {
+    searchInputs.forEach((input) => {
+      const x = input.nextElementSibling;
+      x.addEventListener("click", (event) => {
+        const input = event.target.previousElementSibling;
+        input.value = "";
+      });
+    });
+  }
+
+  /* =====================================================
+       Toggle
+  ===================================================== */
+  const toggles = document.querySelectorAll(".toggle");
+  if (toggles) {
+    toggles.forEach((toggle) => {
+      toggle.addEventListener("click", (event) => {
+        const button = event.target;
+        button.classList.toggle("is-active");
+      });
+    });
+  }
+
+  // 알림 서비스 설정 전체해제
+  const dataToggles = document.querySelectorAll("[data-toggle]");
+  if (dataToggles) {
+    const controllers = document.querySelectorAll(".toggle-controller");
+    controllers.forEach((controller) => {
+      controller.addEventListener("click", (event) => {
+        dataToggles.forEach((toggle) => {
+          if (event.target.dataset.toggle == toggle.dataset.toggle) {
+            toggle.classList = controller.classList;
+          }
+        });
+      });
+    });
+  }
+
+  /* =====================================================
+       Tooltip
+  ===================================================== */
+  const tooltips = document.querySelectorAll(".ico-tooltip");
+  if (tooltips) {
+    tooltips.forEach((tooltip) => {
+      tooltip.addEventListener("click", (event) => {
+        const content = event.target.nextElementSibling;
+        content.classList.toggle("is-active");
+      });
+    });
+  }
+
+  /* =====================================================
+       Checkbox: Check All
+  ===================================================== */
+  const checkAll = document.querySelectorAll(".check-all");
+  if (checkAll) {
+    checkAll.forEach((all) => {
+      const handleCheckAll = (event) => {
+        const inputName = event.target.getAttribute("name");
+        const checkboxes = document.getElementsByName(inputName);
+        checkboxes.forEach((checkbox) => {
+          checkbox.checked = all.checked;
+        });
+      };
+      all.addEventListener("click", handleCheckAll);
+    });
+  }
+
+  /* =====================================================
+       Modal
+  ===================================================== */
+  const modals = document.querySelectorAll(".modal");
+
+  if (modals[0]) {
+    const alertModal = document.querySelector(".modal-alert");
+    const confirmModal = document.querySelector(".modal-confirm");
+    const confirmModalContent = confirmModal.querySelector(".modal-content");
+    const alertModalContent = alertModal.querySelector(".modal-content");
+
+    const initialized = () => {
+      modals.forEach((modal) => {
+        modal.classList.remove("is-active");
+      });
+    };
+
+    const modalButtons = document.querySelectorAll("[data-modal]");
+    if (modalButtons) {
+      const openModal = (event) => {
+        initialized();
+        const modalName = event.target.dataset.modal;
+        const targetModal = document.querySelector(`.${modalName}`);
+        targetModal.classList.add("is-active");
+      };
+
+      modalButtons.forEach((button) => {
+        button.addEventListener("click", openModal);
+      });
+    }
+
+    if (confirmModal) {
+      const modalConfirmButtons = document.querySelectorAll(
+        "[data-modal-confirm]"
+      );
+
+      const openConfirmModal = (event) => {
+        const modalData = event.target.dataset.modalConfirm;
+        const submit = confirmModal.querySelector(".btn-submit");
+        submit.setAttribute("data-modal-alert", modalData);
+        confirmModal.classList.add("is-active");
+
+        const modalAlertButtons =
+          document.querySelectorAll("[data-modal-alert]");
+
+        modalAlertButtons.forEach((button) => {
+          button.addEventListener("click", openAlertModal);
+        });
+
+        if (modalData === "check-delete-keyword") {
+          confirmModalContent.innerHTML =
+            "<p>키워드 삭제 후 적용하시면 삭제된 키워드와 관련된 데이터가 있는 경우 모두 삭제됩니다. 재등록시에도 삭제된 데이터는 복구되지 않습니다. 삭제를 진행하시겠습니까?</p>";
+        }
+
+        if (modalData === "delete-keyword") {
+          confirmModalContent.innerHTML =
+            "<p>소재 삭제 시 해당 소재의 모든 데이터가 삭제됩니다. 재등록시에도 삭제된 데이터는 복구되지 않습니다. 삭제를 진행하시겠습니까?</p>";
+        }
+
+        if (modalData === "check-delete-group") {
+          confirmModalContent.innerHTML =
+            "<p>그룹 삭제 시 그룹에 속한 키워드는 그룹이 해제됩니다. 그룹 삭제를 진행하시겠습니까?</p>";
+        }
+
+        if (modalData === "select-group") {
+          confirmModalContent.innerHTML =
+            "<p>이미 그룹이 지정된 키워드가 있습니다. 그룹을 지정하실 경우 기존 그룹이 새로 지정된 그룹으로 대체됩니다. 그룹지정을 진행하시겠습니까?</p>";
+        }
+      };
+      modalConfirmButtons.forEach((button) => {
+        button.addEventListener("click", openConfirmModal);
+      });
+    }
+
+    const modalAlertButtons = document.querySelectorAll("[data-modal-alert]");
+
+    const openAlertModal = (event) => {
+      alertModal.classList.add("is-active");
+      const modalData = event.target.dataset.modalAlert;
+      if (modalData === "ungroup") {
+        alertModalContent.innerHTML = "<p>그룹해제가 완료되었습니다.</p>";
+      }
+      if (modalData === "create-keyword") {
+        alertModalContent.innerHTML = "<p>키워드 등록이 완료되었습니다.</p>";
+      }
+      if (modalData === "create-group") {
+        alertModalContent.innerHTML = "<p>그룹 등록이 완료되었습니다.</p>";
+      }
+      if (modalData === "search-group") {
+        alertModalContent.innerHTML = "<p>적용이 완료되었습니다.</p>";
+      }
+      if (modalData === "delete-keyword") {
+        alertModalContent.innerHTML = "<p>소재 삭제가 완료되었습니다</p>";
+      }
+      if (modalData === "check-delete-keyword") {
+        alertModalContent.innerHTML = "<p>키워드 삭제가 완료되었습니다</p>";
+      }
+      if (modalData === "check-delete-group") {
+        alertModalContent.innerHTML = "<p>그룹 삭제가 완료되었습니다.</p>";
+      }
+      if (modalData === "select-group") {
+        alertModalContent.innerHTML = "<p>그룹지정이 완료되었습니다.</p>";
+      }
+    };
+    modalAlertButtons.forEach((button) => {
+      button.addEventListener("click", openAlertModal);
+    });
+
+    const closeButtons = document.querySelectorAll(".close");
+    if (closeButtons) {
+      closeButtons.forEach((close) => {
+        close.addEventListener("click", (event) => {
+          const modal = event.target.closest(".modal");
+          modal.classList.remove("is-active");
+
+          if (alertModal) {
+            alertModal.classList.remove("is-active");
+            alertModalContent.innerHTML = "";
+          }
+          if (confirmModal) {
+            confirmModal.classList.remove("is-active");
+            confirmModalContent.innerHTML = "";
+          }
+        });
+      });
+    }
+  }
+
+  /* =====================================================
+       Advanced Search
+  ===================================================== */
+  const advancedSearch = document.querySelector(".advanced-search");
+  if (advancedSearch) {
+    const button = document.querySelector(".btn-advanced-search");
+
+    button.addEventListener("click", () => {
+      advancedSearch.classList.toggle("is-active");
+    });
+  }
+
+  /* =====================================================
+       Section Keyword History: List Scroll
+  ===================================================== */
+  const listScroll = document.querySelector(".list.scroll");
+
+  if (listScroll) {
+    var scrollVertical01 = document.querySelector(".scroll-vertical-01");
+    var scrollVertical02 = document.querySelector(".scroll-vertical-02");
+    var scrollHorizontal01 = document.querySelector(".scroll-horizontal-01");
+    var scrollHorizontal02 = document.querySelector(".scroll-horizontal-02");
+
+    if (scrollVertical01) {
+      const handleScrollVertical = () => {
+        scrollVertical01.scrollTop = scrollVertical02.scrollTop;
+      };
+      scrollVertical02.addEventListener("scroll", handleScrollVertical);
+    }
+
+    if (scrollHorizontal01) {
+      const handleScrollHorizontal = () => {
+        scrollHorizontal01.scrollLeft = scrollHorizontal02.scrollLeft;
+      };
+
+      scrollHorizontal02.addEventListener("scroll", handleScrollHorizontal);
+    }
+  }
+
+  /* =====================================================
+         Section Keyword History: Notification
+    ===================================================== */
+  const notificationDetails = document.querySelector(".notification-details");
+
+  if (notificationDetails) {
+    const notifications = document.querySelectorAll(".notification");
+    notifications.forEach((notification) => {
+      notification.addEventListener("click", (event) => {
+        const details = event.target.nextElementSibling;
+        details.classList.toggle("is-active");
+      });
+    });
+
+    const closeButtons = document.querySelectorAll(".close");
+    closeButtons.forEach((close) => {
+      close.addEventListener("click", (event) => {
+        const modal = event.target.closest("article");
+        modal.classList.remove("is-active");
+      });
+    });
+  }
+
+  /* =====================================================
+         p42: 상품 순위 조회 > 상품등록
+    ===================================================== */
+});
