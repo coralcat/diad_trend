b433888bbe6f7d6a7d900da51bde26994d018e36
Sori Kim <ksr@gabiacns.com>
2022-02-11 11:31:09 +0900

    스크립트 코드 개선, 리스트 셀고정 스크립트에서 CSS로 변경

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,15 +10,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const main = document.querySelector("main");
 
   /* =====================================================
+       Tooltip
+  ===================================================== */
+  // const tooltips = document.querySelectorAll(".tooltip-icon");
+  // if (tooltips) {
+  //   tooltips.forEach((tooltip) => {
+  //     tooltip.addEventListener("click", (event) => {
+  //       const content = event.target.nextElementSibling;
+  //       content.classList.toggle("is-active");
+  //     });
+  //   });
+  // }
+
+  /* =====================================================
        Target Smooth Scroll
   ===================================================== */
+  // 상품 최저가 리포트: 일별 최저가 차트 클릭시
   const anchors = document.querySelectorAll("a[href^='#']");
   if (anchors[0]) {
     anchors.forEach((anchor) => {
       anchor.addEventListener("click", (event) => {
         event.preventDefault();
-        const button = document.querySelector(anchor.getAttribute("href"));
-        button.scrollIntoView({
+        anchor.getAttribute("href").scrollIntoView({
           behavior: "smooth",
         });
       });
@@ -26,8 +39,9 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   /* =====================================================
-       Input Search Close Button
+       Input Search Clear Button
   ===================================================== */
+  // 인풋창에 "x"표시 있을시 clear value
   const searchInputs = document.querySelectorAll("input[type='search'], input[type='url']");
   if (searchInputs) {
     searchInputs.forEach((input) => {
@@ -52,25 +66,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // 알림 서비스 설정 전체해제
+  // 알림 서비스 설정 전체해제 (다른 데에서도 재사용 가능)
   const dataToggles = document.querySelectorAll("[data-toggle]");
   const controllers = document.querySelectorAll(".toggle-controller");
   controllers.forEach((controller) => {
     controller.addEventListener("click", (event) => {
+      const toggleName = event.target.dataset.toggle;
+      const toggles = document.getElementsByName(toggleName);
+
       if (!controller.classList.contains("is-active")) {
         dataToggles.forEach((toggle) => {
-          if (event.target.dataset.toggle === toggle.dataset.toggle) {
-            toggle.classList = controller.classList;
-          }
+          event.target.dataset.toggle === toggle.dataset.toggle && (toggle.classList = controller.classList);
         });
       }
     });
     dataToggles.forEach((toggle) => {
       if (!toggle.classList.contains(".controller")) {
         toggle.addEventListener("click", () => {
-          if (toggle.classList.contains("is-active")) {
-            controller.classList.add("is-active");
-          }
+          toggle.classList.contains("is-active") && controller.classList.add("is-active");
         });
       }
     });
@@ -83,13 +96,10 @@ document.addEventListener("DOMContentLoaded", () => {
     toggles.forEach((toggle) => {
       const inputDisabled = () => {
         const inputs = toggle.parentElement.querySelector(".inputs");
-        if (!toggle.classList.contains("is-active")) {
-          inputs.style.visibility = "hidden";
-        } else {
-          inputs.style.visibility = "visible";
-        }
+        !toggle.classList.contains("is-active")
+          ? (inputs.style.visibility = "hidden")
+          : (inputs.style.visibility = "visible");
       };
-
       inputDisabled();
       toggle.addEventListener("click", inputDisabled);
     });
@@ -98,37 +108,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const detail1 = select[0].closest(".select").querySelector(".details");
     const detail2 = select[1].closest(".select").querySelector(".details");
 
-    select[0].addEventListener("click", () => {
-      detail1.style.opacity = "1";
-      detail2.style.opacity = "0";
-    });
+    select.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        if (event.target === select[0]) {
+          detail1.style.opacity = "1";
+          detail2.style.opacity = "0";
+        } else {
+          detail1.style.opacity = "0";
+          detail2.style.opacity = "1";
+        }
+      });
 
-    select[1].addEventListener("click", () => {
-      detail1.style.opacity = "0";
-      detail2.style.opacity = "1";
+      if (select[0].checked) {
+        detail1.style.opacity = "1";
+        detail2.style.opacity = "0";
+      } else {
+        detail1.style.opacity = "0";
+        detail2.style.opacity = "1";
+      }
     });
-
-    if (select[0].checked) {
-      detail1.style.opacity = "1";
-      detail2.style.opacity = "0";
-    } else {
-      detail1.style.opacity = "0";
-      detail2.style.opacity = "1";
-    }
   }
-
-  /* =====================================================
-       Tooltip
-  ===================================================== */
-  // const tooltips = document.querySelectorAll(".tooltip-icon");
-  // if (tooltips) {
-  //   tooltips.forEach((tooltip) => {
-  //     tooltip.addEventListener("click", (event) => {
-  //       const content = event.target.nextElementSibling;
-  //       content.classList.toggle("is-active");
-  //     });
-  //   });
-  // }
 
   /* =====================================================
        Checkbox: Check All
@@ -163,7 +162,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const alertModalContent = alertModal.querySelector(".modal-content");
     const confirmModalContent = confirmModal.querySelector(".modal-content");
 
-    // Alert Modal
+    // Modal Messages
     const modalMessage = {
       advancedSearch: "검색할 조건이 없습니다.",
       apply: "적용이 완료되었습니다.",
@@ -195,8 +194,8 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     const openModal = (event) => {
+      event.preventDefault();
       const modalData = event.target.dataset.modal;
-      console.log(modalData);
       const targetModal = document.getElementById(modalData);
       if (targetModal) {
         targetModal.classList.add("is-active");
@@ -217,18 +216,14 @@ document.addEventListener("DOMContentLoaded", () => {
         confirmModal.classList.add("is-active");
         const submit = confirmModal.querySelector(".btn-submit");
         for (const property in modalMessage) {
-          if (modalData === `${property}`) {
-            confirmModalContent.innerHTML = `${modalMessage[property]}`;
-          }
+          modalData === `${property}` && (confirmModalContent.innerHTML = `${modalMessage[property]}`);
         }
 
         const openAlertModal = (event) => {
           const modalData = event.target.dataset.modal;
           alertModal.classList.add("is-active");
           for (const property in modalMessage) {
-            if (modalData === `${property}`) {
-              alertModalContent.innerText = `${modalMessage[property]}`;
-            }
+            modalData === `${property}` && (alertModalContent.innerText = `${modalMessage[property]}`);
           }
         };
 
@@ -240,16 +235,14 @@ document.addEventListener("DOMContentLoaded", () => {
       const openAlertModal = () => {
         alertModal.classList.add("is-active");
         for (const property in modalMessage) {
-          if (modalData === `${property}`) {
-            alertModalContent.innerText = `${modalMessage[property]}`;
-          }
+          modalData === `${property}` && (alertModalContent.innerText = `${modalMessage[property]}`);
         }
       };
 
       // 적용, 완료
       if (modalData === "apply") {
-        openAlertModal();
         alertModal.classList.add("modal-apply");
+        openAlertModal();
       }
 
       // 상품 순위 조회: 상품등록팝업 - 조회
@@ -257,49 +250,27 @@ document.addEventListener("DOMContentLoaded", () => {
         const inputs = event.target.closest(".dialog");
         const input = inputs.querySelector("input");
         if (input.value === "") {
-          alertModal.classList.add("is-active");
+          const customModal = document.getElementById(modalData);
+          customModal && customModal.classList.remove("is-active");
           alertModalContent.innerText = "상품 URL을 입력해주세요.";
+          alertModal.classList.add("is-active");
         } else {
           document.querySelector(".search-item").classList.add("is-active");
         }
       }
 
-      // 상품 선택 삭제
-      if (modalData === "deleteItem") {
+      // 조회 목록화면에 상품 선택시 누르는 버튼들
+      if (event.target.closest(".filters")) {
         const listItem = document.querySelector(".list-item");
         const checkboxes = listItem.querySelectorAll("input[type='checkbox']:checked");
         if (checkboxes.length === 0) {
+          const customModal = document.getElementById(modalData);
+          customModal && customModal.classList.remove("is-active");
           alertModal.classList.add("is-active");
           alertModalContent.innerText = "소재를 선택해주세요.";
         } else {
-          openConfirmModal();
-        }
-      }
-
-      // 그룹 지정, 키워드 등록
-      if (modalData === "selectGroup") {
-        const listItem = document.querySelector(".list-item");
-        const checkboxes = listItem.querySelectorAll("input[type='checkbox']:checked");
-        if (checkboxes.length === 0) {
-          const modal = document.querySelector(modalData);
-          modal.classList.remove("is-active");
-          alertModal.classList.add("is-active");
-          alertModalContent.innerText = "소재를 선택해주세요.";
-        } else {
-          openModal();
-        }
-      }
-
-      // 키워드 등록
-      if (modalData === "editKeyword") {
-        const listItem = document.querySelector(".list-item");
-        const checkboxes = listItem.querySelectorAll("input[type='checkbox']:checked");
-        if (checkboxes.length === 0) {
-          document.getElementById(modalData).classList.remove("is-active");
-          alertModal.classList.add("is-active");
-          alertModalContent.innerText = "소재를 선택해주세요.";
-        } else {
-          openModal();
+          modalData === "deleteItem" && openConfirmModal();
+          modalData === "ungroup" && openAlertModal();
         }
       }
 
@@ -327,30 +298,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       }
 
-      // 그룹해제
-      if (modalData === "ungroup") {
-        const listItem = document.querySelector(".list-item");
-        const checkboxes = listItem.querySelectorAll("input[type='checkbox']:checked");
-        if (checkboxes.length === 0) {
-          alertModal.classList.add("is-active");
-          alertModalContent.innerText = "소재를 선택해주세요.";
-        } else {
-          openAlertModal();
-        }
-      }
-
-      // 그룹조회
-      if (modalData === "searchGroup") {
-        const list = document.querySelector(".search-group .list");
-        const checkboxes = list.querySelectorAll("input[type='checkbox']:checked");
-        if (checkboxes.length === 0) {
-          alertModal.classList.add("is-active");
-          alertModalContent.innerText = "그룹을 선택해주세요.";
-        } else {
-          openAlertModal();
-        }
-      }
-
       // 상품 순위 조회: 그룹편집팝업 - 그룹추가
       if (modalData === "createGroup") {
         if (event.target.previousElementSibling.value === "") {
@@ -363,7 +310,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // 상품 순위 조회: 그룹편집팝업 - 선택삭제
       if (modalData === "deleteGroup") {
-        const list = document.querySelector(".edit-group .list");
+        const list = document.querySelector(".modal-edit-group .list");
         const checkboxes = list.querySelectorAll("input[type='checkbox']:checked");
         if (checkboxes.length === 0) {
           alertModal.classList.add("is-active");
@@ -435,9 +382,7 @@ document.addEventListener("DOMContentLoaded", () => {
         confirmModalContent.innerHTML = "";
       }
 
-      if (modal.classList.contains("modal-apply")) {
-        initialize();
-      }
+      modal.classList.contains("modal-apply") && initialize();
     };
 
     const closeButtons = document.querySelectorAll(".close");
@@ -458,11 +403,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const checkboxes = advancedSearch.querySelectorAll("input[type='checkbox']");
     const selections = advancedSearch.querySelectorAll(".select input:first-of-type");
 
-    button.addEventListener("click", () => {
-      initialize();
-      advancedSearch.classList.toggle("is-active");
-    });
-
     //초기화
     const initialize = () => {
       inputs.forEach((input) => {
@@ -477,6 +417,13 @@ document.addEventListener("DOMContentLoaded", () => {
         select.checked = true;
       });
     };
+
+    button.addEventListener("click", () => {
+      initialize();
+      advancedSearch.classList.toggle("is-active");
+    });
+
+    // 초기화
     const clear = advancedSearch.querySelector(".clear");
     clear.addEventListener("click", initialize);
 
@@ -487,11 +434,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     options.forEach((option) => {
       const choose = (event) => {
-        if (event.target.dataset.select === "yes") {
-          calendar.classList.add("is-active");
-        } else {
-          calendar.classList.remove("is-active");
-        }
+        event.target.dataset.select === "yes"
+          ? calendar.classList.add("is-active")
+          : calendar.classList.remove("is-active");
       };
       option.addEventListener("click", choose);
     });
@@ -500,27 +445,28 @@ document.addEventListener("DOMContentLoaded", () => {
   /* =====================================================
        Section Keyword History: List Scroll
   ===================================================== */
-  const listScroll = document.querySelector(".list.scroll");
+  // css로 해결
+  // const listScroll = document.querySelector(".list.scroll");
 
-  if (listScroll) {
-    var scrollVertical01 = document.querySelector(".scroll-vertical-01");
-    var scrollVertical02 = document.querySelector(".scroll-vertical-02");
-    var scrollHorizontal01 = document.querySelector(".scroll-horizontal-01");
-    var scrollHorizontal02 = document.querySelector(".scroll-horizontal-02");
+  // if (listScroll) {
+  //   var scrollVertical01 = document.querySelector(".scroll-vertical-01");
+  //   var scrollVertical02 = document.querySelector(".scroll-vertical-02");
+  //   var scrollHorizontal01 = document.querySelector(".scroll-horizontal-01");
+  //   var scrollHorizontal02 = document.querySelector(".scroll-horizontal-02");
 
-    if (scrollVertical01) {
-      const handleScrollVertical = () => {
-        scrollVertical01.scrollTop = scrollVertical02.scrollTop;
-      };
-      scrollVertical02.addEventListener("scroll", handleScrollVertical);
-    }
+  //   if (scrollVertical01) {
+  //     const handleScrollVertical = () => {
+  //       scrollVertical01.scrollTop = scrollVertical02.scrollTop;
+  //     };
+  //     scrollVertical02.addEventListener("scroll", handleScrollVertical);
+  //   }
 
-    if (scrollHorizontal01) {
-      const handleScrollHorizontal = () => {
-        scrollHorizontal01.scrollLeft = scrollHorizontal02.scrollLeft;
-      };
+  //   if (scrollHorizontal01) {
+  //     const handleScrollHorizontal = () => {
+  //       scrollHorizontal01.scrollLeft = scrollHorizontal02.scrollLeft;
+  //     };
 
-      scrollHorizontal02.addEventListener("scroll", handleScrollHorizontal);
-    }
-  }
+  //     scrollHorizontal02.addEventListener("scroll", handleScrollHorizontal);
+  //   }
+  // }
 });
