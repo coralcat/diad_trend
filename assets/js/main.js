12c32c1902a5bd76e1dee9dde18d03af82c0e97d
김소리 <ksr@gabiacns.com>
2023-04-04 11:49:18 +0900

    Update 2023-04-04T02:49:12.237Z

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 
   window.addEventListener("resize", () => {
-    let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   });
 
